ac6e58efe30c6a0d47210fc4177e5ea77ac8a7b1
Giorgio Lucca <giorgiolucca@hotmail.com>
2023-10-05 00:52:50 -0300

    v0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,114 @@
-export * as sample from "./sample";
+#!/usr/bin/env node
+
+import fs from 'fs';
+import npmLogin from 'npm-cli-login';
+import child_process from 'child_process';
+import yargs from 'yargs/yargs';
+import gitignoreParser from 'gitignore-parser';
+import retry from 'retry';
+import { rimrafSync } from 'rimraf';
+import { hideBin } from 'yargs/helpers';
+
+const npmrcsCall = (npmrcsDirectory: string) => {
+  const command = `NPMRC_STORE=${npmrcsDirectory} NPMRC=.npmrc npx npmrc`;
+
+  try {
+    child_process.execSync(command);
+  } catch (e) {}
+}
+
+const npmrcsCreateProfile = (npmrcsDirectory: string, profile: string) => {
+  const command = `NPMRC_STORE=${npmrcsDirectory} NPMRC=.npmrc npx npmrc -c ${profile}`;
+
+  try {
+    child_process.execSync(command);
+  } catch (e) {}
+}
+
+const npmrcsChangeProfile = (npmrcsDirectory: string, profile: string) => {
+  const command = `NPMRC_STORE=${npmrcsDirectory} NPMRC=.npmrc npx npmrc ${profile}`;
+
+  try {
+    child_process.execSync(command);
+  } catch (e) {}
+}
+
+const retryGetNpmFile = (path: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const operationOptions = {
+      retries: 5,
+      factor: 3
+    };
+    const operation = retry.operation(operationOptions);
+      operation.attempt((currentAttempt) => {
+        if (! fs.existsSync(path)) {
+          operation.retry(new Error('File not exists'));
+        } else {
+          if (currentAttempt > operationOptions.retries) {
+            operation.mainError();
+          } else {
+            resolve(fs.readFileSync(path).toString());
+          }
+        }
+      });
+  });
+}
+
+(async() => {
+  const argv: any = yargs(hideBin(process.argv)).argv
+
+  if (! argv.token) {
+    throw new Error('Token is required');
+  }
+  const token = argv.token;
+
+  if (! argv.scope) {
+    throw new Error('Scope is required');
+  }
+  const scope = argv.scope;
+
+  if (! argv.registry) {
+    throw new Error('Registry is required');
+  }
+  const registry = argv.registry;
+
+  const localUsername = argv.localUsername || 'verdaccio';
+  const localPassword = argv.localPassword || 'verdaccio';
+  const localEmail = argv.localEmail || 'test@verdaccio.com';
+  const localRegistry = argv.localRegistry || 'http://127.0.0.1:4873';
+
+  const npmrcsDirectory = '.npmrcs';
+
+  if (fs.existsSync(npmrcsDirectory)) {
+    rimrafSync(npmrcsDirectory);
+  }
+
+  if (fs.existsSync('.npmrc')) {
+    fs.rmSync('.npmrc');
+  }
+  npmrcsCall(npmrcsDirectory);
+  npmrcsCreateProfile(npmrcsDirectory, 'local');
+
+  const gitignorePath = '.gitignore';
+  const gitignore = gitignoreParser.compile(fs.readFileSync(gitignorePath, 'utf8'));
+
+  if (gitignore.accepts(npmrcsDirectory) === true) {
+    fs.appendFileSync(gitignorePath, '\n.npmrcs');
+  }
+  npmLogin(localUsername, localPassword, localEmail, localRegistry, scope, false, '.npmrc_local');
+
+  const npmrcLocal: string = await retryGetNpmFile('.npmrc_local');
+  fs.writeFileSync(`${npmrcsDirectory}/local`, npmrcLocal);
+
+  npmrcsChangeProfile(npmrcsDirectory, 'default');
+
+  npmLogin(localUsername, token, localEmail, registry, scope, false, '.npmrc_default');
+  const npmrcDefault = await retryGetNpmFile('.npmrc_default');
+
+  fs.writeFileSync(`${npmrcsDirectory}/default`, npmrcDefault);
+
+  rimrafSync('.npmrc_local');
+  rimrafSync('.npmrc_default');
+
+  npmrcsChangeProfile(npmrcsDirectory, 'local');
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: "./src/index.ts",
+  target: ["node"],
   output: {
     libraryTarget: "commonjs2",
     path: path.join(__dirname, "dist"),
